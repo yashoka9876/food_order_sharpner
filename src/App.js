e9953d8bb52c87ceacd332fecbdddf177e60824c
AshokYadav <yashoka51@gmail.com>
2024-01-09 01:40:33 +0530

    add array of items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import Headers from "./components/Layout/Headers";
 
+let DUMMY_ITEMS=[
+  {
+    name:'Shushi',
+    description:'Finest fish and veggie',
+    prize:'$11.99'
+  },
+  {
+    name:'Schitzel',
+    description:'A german specialty',
+    prize:'$16.99'
+  },
+  {
+    name:'Barbecue Burger',
+    description:'American, raw,meaty',
+    prize:'$19.44'
+  },
+  {
+    name:'Green Bowl',
+    description:'Healthy...and green...',
+    prize:'$22.33'
+  }
+]
 
 function App() {
   return (
     <>
       <Headers />
+      <ul>
+        {
+          DUMMY_ITEMS.map((item)=>{
+            return <li>
+              <span>{item.name}</span>
+              <span>{item.description}</span>
+              <span>{item.prize}</span>
+            </li>
+          })
+        }
+      </ul>
     </>
   );
 }
--- src/components/Layout/Headers.js
+++ src/components/Layout/Headers.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = () => {
   return (
     <>
     <header className={classes.header}>
       <h1>ReactMeals</h1>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
       <img src={mealsImage} alt='A table of delicious food!' />
@@ -17,6 +18,7 @@ const Header = () => {
       <p> Choose your favourite from our broad selection of available meals and enjoy a delicious lunch of dinner at home. </p>
       <p>All our meals are cooked with high-quality ingredients,just-in-time and of couse by experienced chefs!</p>
     </div>
+    
     </>
   )
 }
